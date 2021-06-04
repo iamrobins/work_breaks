d7d5cf0fac191a31efd5fdf1aea0db08fde0f2a7
itsOwen <owensingh72@gmail.com>
2021-06-05 02:56:23 +0530

    added toast

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 import datetime
 from playsound import playsound
+from notifypy import Notify
+
+notification = Notify()
 
 def main():
-  print("Simple break reminder script\n")
-  pref_time = prefered_time_inp()
-  print("Script Started !!")
-  break_reminder(pref_time)
+    print("Simple break reminder script\n")
+    pref_time = prefered_time_inp()
+    print("Script Started !!")
+    break_reminder(pref_time)
+
 
 def prefered_time_inp():
-  while True:
-    try:
-      pref_time = 60 * int(input("Enter your break preference in minutes: "))
-      return pref_time
-    except:
-      print("Please provide a valid value “for example 20” ")
-      continue
+    while True:
+        try:
+            pref_time = 60 * \
+                int(input("Enter your break preference in minutes: "))
+            return pref_time
+        except:
+            print("Please provide a valid value “for example 20” ")
+            continue
+
 
 def break_reminder(pref_time):
-  last_time = datetime.datetime.now()
+    last_time = datetime.datetime.now()
 
-  while True:
-    diff = (datetime.datetime.now() - last_time).total_seconds()
-    if diff > pref_time : 
-      print("Hey, I am your assistant please take a break.")
-      try:
-        playsound("speech.mp3")
-      except:
-        print("No speech found")
-      last_time = datetime.datetime.now()
+    while True:
+        diff = (datetime.datetime.now() - last_time).total_seconds()
+        if diff > pref_time:
+            print("Hey, I am your assistant please take a break.")
+            try:
+                notification.title = "Break Time!!"
+                notification.message = "Do you know taking little breaks between your work is way of improving your efficiency."
+                notification.icon = "logo.png"
+                notification.send(block=False)
+                playsound("speech.mp3")
+            except:
+                print("No speech found")
+            last_time = datetime.datetime.now()
 
 
 # Run Script
