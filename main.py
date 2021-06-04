12e6832c6efe161ae3bc431649bc2175ad738b77
iamrobins <itsrobin4@gmail.com>
2021-06-04 20:48:59 +0530

    sleep detection

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ while True:
   time.sleep(1)
   diff = (datetime.datetime.now() - curr_time).total_seconds()
   print(diff)
+
+  # When PC/Laptop is put to sleep
   if diff > 10: 
     print("I just wake up after sleep") 
     break
