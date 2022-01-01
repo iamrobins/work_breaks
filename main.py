361f5eea233e6635e54f0d5a776e803f9e681425
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2022-01-01 23:18:43 +0530

    Fix Issue #4 - Turn AppState into a class with properties - Add thread lock to concurrently accessed AppState - Wrap playsound() around safe_playsound()

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import threading
 from playsound import playsound
 from notifypy import Notify
 from custom_types import AppState
-from sys import exit
+from sys import exit, stderr
 
 notification = Notify()
 continue_reminder_time: int = 300 #5 minutes
@@ -36,15 +36,21 @@ def prefered_time_inp() -> int:
             print("Please provide a valid value “for example 20” ")
         except KeyboardInterrupt:
             print("\nExiting program")
-            exit(1)
+            exit()
+
+def safe_playsound(path: str):
+    try:
+        playsound(path)
+    except:
+        print(f"Could not play sound: {path}", file=stderr)
 
 
 def continue_reminder(state) -> None:
     """Reminds about coming back to work after break"""
     time.sleep(continue_reminder_time)
-    while(not state["break_over"]):
+    while(not state.break_over and not state.quit):
         send_notification(title="Continue Reminder", message="Don't forget to get back to work after the break", icon="images/logo.png")
-        playsound("sound/countinue_work.mp3")
+        safe_playsound("sound/countinue_work.mp3")
         time.sleep(continue_reminder_time)
 
 
@@ -56,21 +62,22 @@ def break_reminder(pref_time: int) -> None:
         diff = (datetime.datetime.now() - last_time).total_seconds()
         if diff > pref_time:
             print("Hey, I am your assistant please take a break.")
+            send_notification(title="Break Time!!", message="Do you know taking little breaks between your work is way of improving your efficiency.", icon="images/logo.png")
+            safe_playsound("sound/take_break.mp3")
+            # Continue Work
+            state: AppState = AppState(break_over=False)
+            continue_reminder_t: threading.Thread = threading.Thread(target=continue_reminder, args=(state,))
+            continue_reminder_t.start()
             try:
-                send_notification(title="Break Time!!", message="Do you know taking little breaks between your work is way of improving your efficiency.", icon="images/logo.png")
-                playsound("sound/take_break.mp3")
-
-                # Countinue Work
-                state: AppState = {
-                    "break_over": False
-                }
-                continue_reminder_t: threading.Thread = threading.Thread(target=continue_reminder, args=(state,))
-                continue_reminder_t.start()
-                input("Did you take the break?\nPress any key to continue.")
-                state["break_over"] = True
-                print("You are doing good. Keep up")
-            except:
-                print("No speech found")
+                input("Did you take the break?\nPress Enter to continue or Ctrl-C to quit.\n")
+            except KeyboardInterrupt:
+                print("\nExiting program")
+                state.quit = True
+                exit()
+            state.break_over = True
+            #Verify that thread has actually stopped
+            continue_reminder_t.join()
+            print("You are doing good. Keep it up")
             last_time = datetime.datetime.now()
 
 if __name__ == "__main__":
